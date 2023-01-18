7f644a4a95c88768682ceeacf4d3631ad6c3beab
Gimahni Harshika <89870885+GimhaniHM@users.noreply.github.com>
2023-01-19 04:39:33 +0530

    Added footer icons

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -3,6 +3,11 @@ import './home.scss'
 import video from '../../Assest/landingPage.mp4'
 import {GrLocation} from 'react-icons/gr'
 import {HiFilter} from 'react-icons/hi'
+import {FiFacebook} from 'react-icons/fi'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {FaTripadvisor} from 'react-icons/fa'
+import {BsListTask} from 'react-icons/bs'
+import {TbApps} from 'react-icons/tb'
 
 const Home = () => {
     return (
@@ -56,6 +61,19 @@ const Home = () => {
 
                     </div>
 
+                </div>
+
+                <div className="homeFooterIcons flex">
+                    <div className="rightIcons">
+                        <FiFacebook className="icon"/>
+                        <AiOutlineInstagram className="icon"/>
+                        <FaTripadvisor className="icon"/>
+                    </div>
+
+                    <div className="leftIcons">
+                        <BsListTask className="icon"/>
+                        <TbApps className="icon"/>
+                    </div>
                 </div>
             </div>
         
